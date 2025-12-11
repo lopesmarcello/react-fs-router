902db15950e8e36e86e515c33e041fdade6e168c
Marcello Lopes <marcellolopesdev@gmail.com>
2025-12-11 07:48:24 -0300

    config update

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
 export { FileRouter } from "./components/FileRouter";
 export { createFileRouter } from "./core/create-file-router";
-export { useRouter } from "./hooks/useRouter";
-export { useParams } from "./hooks/useParams";
-
-// Re-exports
-export {
-  Link,
-  NavLink,
-  Outlet,
-  Navigate,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
 
 export type { FileRouterProps, RouteModule } from "./types";
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { FileRouter } from "./components/FileRouter.tsx";
 
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <FileRouter />
